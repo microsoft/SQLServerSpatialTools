9c36ef164a9b1ee35dae653339aa1d40b2320441
isaac_cp <SND\isaac_cp>
2008-07-25 16:11:52 +0000

    Initial submission.
    Includes code for geometry shift, and locate along for geometry and geography linestrings.

---
+++ Functions.cs
@@ -0,0 +1,163 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SqlServer.Types;
+
+namespace SQLSpatialTools
+{
+    /**
+     * This class contains functions that can be registered in SQL Server.
+     */
+    public class Functions
+    {
+        const double THRESHOLD = .01;  // 1 cm tolerance in most SRIDs
+
+
+        // Make our ShiftGeometrySink into a function call by hooking it into a simple pipeline.
+        public static SqlGeometry ShiftGeometry(SqlGeometry g, double xShift, double yShift)
+        {
+            // create a sink that will create a geometry instance
+            SqlGeometryBuilder b = new SqlGeometryBuilder();
+
+            // create a sink to do the shift and plug it in to the builder
+            ShiftGeometrySink s = new ShiftGeometrySink(xShift, yShift, b);
+
+            // plug our sink into the geometry instance and run the pipeline
+            g.Populate(s);
+
+            // the end of our pipeline is now populated with the shifted geometry instance
+            return b.ConstructedGeometry;
+        }
+
+        // Make our LocateAlongGeographySink into a function call.  This function just hooks up
+        // and runs a pipeline using the sink.
+        public static SqlGeography LocateAlongGeog(SqlGeography g, double distance)
+        {
+            SqlGeographyBuilder b = new SqlGeographyBuilder();
+            LocateAlongGeographySink p = new LocateAlongGeographySink(distance, b);
+            g.Populate(p);
+            return b.ConstructedGeography;
+        }
+
+
+        // Make our LocateAlongGeometrySink into a function call.  This function just hooks up
+        // and runs a pipeline using the sink.
+        public static SqlGeometry LocateAlongGeom(SqlGeometry g, double distance)
+        {
+            SqlGeometryBuilder b = new SqlGeometryBuilder();
+            LocateAlongGeometrySink p = new LocateAlongGeometrySink(distance, b);
+            g.Populate(p);
+            return b.ConstructedGeometry;
+        }
+
+
+
+        // Find the point that is the given distance from the start point in the direction of the end point.
+        // The distance must be less than the distance between these two points.
+        public static SqlGeography InterpolateBetweenGeog(SqlGeography start, SqlGeography end, double distance)
+        {
+            // We need to check a few prequisites.
+
+            // We only operate on points.
+
+            if (start.STGeometryType().Value != "Point")
+            {
+                throw new ArgumentException("Start value must be a point.");
+            }
+
+            if (end.STGeometryType().Value != "Point")
+            {
+                throw new ArgumentException("Start value must be a point.");
+            }
+
+            // The SRIDs also have to match
+            int srid = start.STSrid.Value;
+            if (srid != end.STSrid.Value)
+            {
+                throw new ArgumentException("The start and end SRIDs must match.");
+            }
+
+            // Finally, the distance has to fall between these points.
+            if (distance > start.STDistance(end))
+            {
+                throw new ArgumentException("The distance value provided exceeds the distance between the two points.");
+            }
+            else if (distance < 0)
+            {
+                throw new ArgumentException("The distance must be positive.");
+            }
+
+            // We'll just do this by binary search---surely this could be more efficient, but this is 
+            // relatively easy.
+            //
+            // Note that we can't just take the take the linear combination of end vectors because we
+            // aren't working on a sphere.
+
+            // We are going to do our binary search using 3D Cartesian values, however
+            double[] startCart = Util.GeographicToCartesian(start);
+            double[] endCart = Util.GeographicToCartesian(end);
+            double[] currentCart;
+
+            SqlGeography current;
+            double currentDistance;
+
+            // Keep refining until we slip below the THRESHOLD value.
+            do
+            {
+                currentCart = Util.SphericalMidpoint(startCart, endCart);
+                current = Util.CartesianToGeographic(currentCart, srid);
+                currentDistance = start.STDistance(current).Value;
+                if (distance <= currentDistance) endCart = currentCart;
+                else startCart = currentCart;
+            } while (Math.Abs(currentDistance - distance) > THRESHOLD);
+
+            return current;
+        }
+
+        // Find the point that is the given distance from the start point in the direction of the end point.
+        // The distance must be less than the distance between these two points.
+        public static SqlGeometry InterpolateBetweenGeom(SqlGeometry start, SqlGeometry end, double distance)
+        {
+            // We need to check a few prequisites.
+
+            // We only operate on points.
+
+            if (start.STGeometryType().Value != "Point")
+            {
+                throw new ArgumentException("Start value must be a point.");
+            }
+
+            if (end.STGeometryType().Value != "Point")
+            {
+                throw new ArgumentException("Start value must be a point.");
+            }
+
+            // The SRIDs also have to match
+            int srid = start.STSrid.Value;
+            if (srid != end.STSrid.Value)
+            {
+                throw new ArgumentException("The start and end SRIDs must match.");
+            }
+
+            // Finally, the distance has to fall between these points.
+            double length = start.STDistance(end).Value;
+            if (distance > start.STDistance(end))
+            {
+                throw new ArgumentException("The distance value provided exceeds the distance between the two points.");
+            }
+            else if (distance < 0)
+            {
+                throw new ArgumentException("The distance must be positive.");
+            }
+
+            // Since we're working on a Cartesian plane, this is now pretty simple.
+            double f = distance/length;  // The fraction of the way from start to end.
+            double newX = (start.STX.Value * (1-f)) + (end.STX.Value * f);
+            double newY = (start.STY.Value * (1-f)) + (end.STY.Value * f);
+            return SqlGeometry.Point(newX, newY, srid);
+        }
+    }
+}
---
+++ ShiftGeometrySink.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+
+using Microsoft.SqlServer.Types;
+
+namespace SQLSpatialTools
+{
+    /**
+     * This class implements a geometry sink that will shift an input geometry by a given amount in the x and
+     * y directions.  It directs its output to another sink, and can therefore be used in a pipeline if desired.
+     */
+    public class ShiftGeometrySink : IGeometrySink
+    {
+        IGeometrySink _target;  // the target sink
+        double _xShift;         // How much to shift in the x direction.
+        double _yShift;         // How much to shift in the y direction.
+
+
+        // We take an amount to shift in the x and y directions, as well as a target sink, to which
+        // we will pipe our result.
+        public ShiftGeometrySink(double xShift, double yShift, IGeometrySink target)
+        {
+            _target = target;
+            _xShift = xShift;
+            _yShift = yShift;
+        }
+
+        // Just pass through without change.
+        public void SetSrid(int srid)
+        {
+            _target.SetSrid(srid);
+        }
+
+        // Just pass through without change.
+        public void BeginGeometry(OpenGisGeometryType type)
+        {
+            _target.BeginGeometry(type);
+        }
+
+        // Each BeginFigure call will just move the start point by the required amount.
+        public void BeginFigure(double x, double y, double? z, double? m)
+        {
+            _target.BeginFigure(x + _xShift, y + _yShift, z, m);
+        }
+
+        // Each AddLine call will just move the endpoint by the required amount.
+        public void AddLine(double x, double y, double? z, double? m)
+        {
+            _target.AddLine(x + _xShift, y + _yShift, z, m);
+        }
+
+        // Just pass through without change.
+        public void EndFigure()
+        {
+            _target.EndFigure();
+        }
+
+        // Just pass through without change.
+        public void EndGeometry()
+        {
+            _target.EndGeometry();
+        }
+    }
+}
